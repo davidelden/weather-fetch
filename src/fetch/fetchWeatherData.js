92e2ee656df81f49f851d1364b2f3d8d25ae687f
davidelden <deelden+github@gmail.com>
2019-10-21 13:42:24 -0500

    add functions [fetchFromAPI, saveWeatherData]

--- src/fetch/fetchWeatherData.js
+++ src/fetch/fetchWeatherData.js
@@ -1,4 +1,5 @@
-const writeStream = require('../streams/actions/writeStream.js'),
+const https = require('https'),
+      writeStream = require('../streams/actions/writeStream.js'),
       eventMessages = require('../streams/events/eventMessages.js'),
       db = require('../db/connection.js'),
       streamName = 'WeatherFetch';
@@ -21,15 +22,14 @@ const fetchWeatherData = async msg => {
   // Publish start message to Redis stream
   writeStream(streamName, eventMessages['start']);
 
-  const dbTable = fetchMsgTimeZones[msg],
-        zipCodes = await fetchZipCodes(dbTable);
+  const dbTbl = fetchMsgTimeZones[msg],
+        zipCodes = await fetchZipCodes(dbTbl);
 
-  // Begin fetching weather data
-  zipCodes.forEach(code => {
-    const endPoint = weatherAPIUrl(code);
-    console.log('endPoint:', endPoint);
+  zipCodes.forEach(zipCode => {
+    const endPoint = weatherAPIUrl(zipCode);
+
+    fetchFromAPI(endPoint, dbTbl, zipCode);
   });
-  // Emit success/error message to stream
 
   // Publish end message to Redis stream
   writeStream(streamName, eventMessages['end']);
@@ -44,8 +44,62 @@ const fetchZipCodes = tbl => (
     .catch(err => console.error('error:', err))
 )
 
-const saveWeatherData = (data, zipCode) => {
-  // save fetched weather data into database
+const fetchFromAPI = (url, dbTbl, zipCode) => {
+  https.get(url, res => {
+    const { statusCode } = res,
+          contentType = res.headers['content-type'];
+
+    let error,
+        rawData = '';
+
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+
+    if (error) {
+      writeStream(streamName, eventMessages['error'](error.message));
+      console.error(error.message);
+      // consume response data to free up memory
+      res.resume();
+      return;
+    }
+
+    res.setEncoding('utf8');
+    res.on('data', chunk => rawData += chunk);
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData),
+              weatherData = { data: parsedData.data.slice(0,1) };
+
+        saveWeatherData(dbTbl, zipCode, weatherData);
+      } catch (err) {
+        writeStream(streamName, eventMessages['error'](err.message));
+        console.error(err.message);
+      }
+    });
+  })
+  .on('error', err => {
+    writeStream(streamName, eventMessages['error'](err.message));
+    console.error(`https.get received an error: ${err.message}`);
+  });
+}
+
+const saveWeatherData = (dbTbl, zipCode, data) => {
+  db(dbTbl)
+    .where({ zip_code: zipCode })
+    .update({ data: data })
+    .then(() => {
+      writeStream(streamName, eventMessages['data'](zipCode));
+      console.log(`${dbTbl} updated with ${zipCode} weather data`);
+    })
+    .catch(err => {
+      writeStream(streamName, eventMessages['error'](err));
+      console.error('error:', err);
+    })
 }
 
 module.exports = fetchWeatherData;

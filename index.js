62906b862b09b566d934e432c6b791d3dd9057da
davidelden <deelden+github@gmail.com>
2019-10-22 09:25:02 -0500

    start server in index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const migrateLatest = require('./src/db/migrateLatest.js'),
       readStream = require('./src/streams/actions/readStream.js'),
       msgEmitter = require('./src/emitter/msgEmitter'),
       fetchWeatherData = require('./src/fetch/fetchWeatherData'),
-      streamName = 'StartWeatherFetch';
+      streamName = 'StartWeatherFetch',
+      startServer = require('./src/api/server.js');
 
 migrateLatest();
 readStream(streamName);
-msgEmitter.on('streamMessage', msg => fetchWeatherData(msg));
+msgEmitter.on('streamMessage', msg => fetchWeatherData(msg)); // Check if msg is a valid msg
+startServer();
